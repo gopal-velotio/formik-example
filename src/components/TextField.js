e037d374e3433235b82db82fd6ecbcb1ad044be3
gopal-velotio <gopal.zalke@velotio.com>
2021-08-04 18:21:36 +0530

    Sign up form added with formik

---
+++ src/components/TextField.js
@@ -0,0 +1,21 @@
+import { ErrorMessage, useField } from 'formik';
+
+const TextField = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className='mb-2'>
+      <label htmlFor={field.name} className="mt-2">{label}</label>
+      <input
+        autoComplete='off'
+        className={`form-control shadow-none ${
+          meta.touched && meta.error && 'is-invalid'
+        }`}
+        {...field}
+        {...props}
+      />
+      <ErrorMessage component='div' className='error' name={field.name} />
+    </div>
+  );
+};
+
+export default TextField;
